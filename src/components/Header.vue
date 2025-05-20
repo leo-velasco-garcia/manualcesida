2e38864648a09e40316b0761af46f1f51b6236cd
Leo Velasco García <leo@172-15-25-39.lightspeed.hstntx.sbcglobal.net>
2025-05-20 09:42:19 +0200

    ventanas

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+    let num = Math.floor(Math.random() * 38) + 1;
+    const logoheader = new URL(`../assets/header/LogoCesida-${num}.svg`, import.meta.url).href
+</script>
+<template>
+    <header class="header">
+        <img :src="logoheader" alt="Logotipo de Cesida">
+    </header>
+</template>
+<style scoped>
+    .header{
+        background-color: #ffffff;
+        position: fixed;
+        right: 0;
+        top: 0;
+        width: calc(100vw - 20rem);
+        height: 5rem;
+        z-index: 999;
+    }
+    .header img{
+        height: 1.5rem;
+        position: absolute;
+        right: 1.75rem;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+<template>
+    <nav class="nav">
+    </nav>
+</template>
+<style scoped>
+    .nav{
+        background-color: #F18C79;
+        position: fixed;
+        left: 0;
+        top: 0;
+        height: 100vh;
+        width: 20rem;
+        display: flex;
+        flex-flow: column nowrap;
+        gap: 1rem;
+        padding: 1.75rem;
+    }
+
+    .nav .grupoNav{
+        display: flex;
+        flex-flow: column nowrap;
+        /* border: 1px red solid; */
+    }
+</style>
